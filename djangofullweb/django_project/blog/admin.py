b2413f8c417f05b130948c0e55de5809d32f75db
Gang <ganglix@gmail.com>
2020-08-28 23:19:48 -0600

    after database and migration

--- djangofullweb/django_project/blog/admin.py
+++ djangofullweb/django_project/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register your models here. so they show up on the admin page
+admin.site.register(Post)
--- djangofullweb/django_project/blog/models.py
+++ djangofullweb/django_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here. to be saved to the data base
+
+class Post(models.Model):  # inheriate 
+    title = models.CharField(max_length=100)
+    content = models.TextField()  # unrestricted text
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # user to post, one to many, if the user is deleted the model is deleted, not the way around
+
+    def __str__(self):
+        return self.title
