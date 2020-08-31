cb20dcbe2d003b9d2e2dfa49af922dbbd4c89b54
Gang <ganglix@gmail.com>
2020-08-31 11:20:59 -0600

    update image and resize

--- djangofullweb/django_project/users/models.py
+++ djangofullweb/django_project/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 # Create your models here.
 
 # one to one 
@@ -9,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):  # this save is to overwrite the parent class to add more functionality
+        super().save()  # parent method
+
+        img = Image.open(self.image.path)  # open image of the current instance
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)  # overwrite
--- djangofullweb/django_project/users/views.py
+++ djangofullweb/django_project/users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm # a class inherit from UserCreationForm
+# UserRegisterForm # a class inherit from UserCreationForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 # Create your views here.
 def register(request):
@@ -21,4 +23,25 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            username = u_form.cleaned_data.get('username')
+            messages.success(request, f'You account for {username} is updated')
+            return redirect('profile')
+
+            
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    # pass to template
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
